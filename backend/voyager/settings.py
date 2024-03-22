5932e22acaeef85b555e94bfd15245fbf19926af
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2024-03-22 15:15:49 +0530

    Update ALLOWED_HOSTS in settings.py

--- backend/voyager/settings.py
+++ backend/voyager/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ouwoe74c$=jyi@x^5-8hw)tj&pb^jak4g1lt4)ys1*y%yqlfnz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
