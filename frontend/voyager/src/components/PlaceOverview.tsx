e42ea9c7fb6cf398934a628f37db7a460f0a60ec
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2024-04-01 12:02:29 +0530

    Updated wishlist button

--- frontend/voyager/src/components/PlaceOverview.tsx
+++ frontend/voyager/src/components/PlaceOverview.tsx
@@ -114,30 +114,36 @@ const PlaceOverview = () => {
             <p className="text-3xl font-semibold text-center">
               {overview.cost}
             </p>
-            <div className="flex justify-center items-center hover:bg-[#f37979] bg-opacity-40 rounded-full transition-all z-20 mx-3 p-1">
-              <p>Add to Wishlist </p>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className={`w-6 h-6 hover:text-red-600 m-1  cursor-pointer `}
-                onClick={() =>
-                  toast.promise(addToWishlist(card), {
-                    loading: "Adding to Wishlist...",
-                    success: (message) => `${message}`,
-                    error: (err) => ` ${err}`,
-                  })
-                }
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                />
-              </svg>
-            </div>
+
+            <button
+              className="p-2 m-2 border rounded-md bg-white border-red-500 hover:bg-red-500 hover:text-white transition-all duration-300 shadow-md"
+              onClick={() =>
+                toast.promise(addToWishlist(card), {
+                  loading: "Adding to Wishlist...",
+                  success: (message) => `${message}`,
+                  error: (err) => ` ${err}`,
+                })
+              }
+            >
+              <div className="flex items-center">
+                <p>Wishlist!</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className={`w-6 h-6 hover:text-red-600 m-1  cursor-pointer `}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                  />
+                </svg>
+              </div>
+            </button>
+
             <div className="">
               <button
                 className="p-2 m-2 border rounded-md bg-white border-green-300 hover:bg-green-300 hover:text-white transition-all duration-300 shadow-md"
